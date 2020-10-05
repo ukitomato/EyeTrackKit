59844b9464c8959b9c69c2c5f48b987f9bda4b56
Yuki Yamato <ukitomato.developer@gmail.com>
2020-10-06 00:02:48 +0900

    Fixed sceneView size

--- Sources/EyeTrackKit/EyeTrackView.swift
+++ Sources/EyeTrackKit/EyeTrackView.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARVideoKit
 
 public struct EyeTrackView: UIViewRepresentable {
-    @State public var sceneView: ARSCNView = ARSCNView(frame: .zero)
+    @State public var sceneView: ARSCNView = ARSCNView(frame: UIScreen.main.bounds)
     public var eyeTrack: EyeTrack
     public var recorder: RecordAR?
     private var isHidden: Bool
